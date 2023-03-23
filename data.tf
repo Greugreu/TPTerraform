e25b3233e21a969be132295fa48c895f7e051380
Greugreu <fenris.fitz@hotmail.fr>
2023-03-23 15:19:25 +0100

    pgadmin data && api container && pgadmin container

--- data.tf
+++ data.tf
@@ -16,3 +16,13 @@ data "azurerm_key_vault_secret" "postgres-password" {
   key_vault_id = data.azurerm_key_vault.kv.id
   name         = "postgres-password"
 }
+
+data "azurerm_key_vault_secret" "pgadmin-username" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  name         = "pgadmin-username"
+}
+
+data "azurerm_key_vault_secret" "pgadmin-password" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  name         = "pgadmin-password"
+}
--- main.tf
+++ main.tf
@@ -34,33 +34,32 @@ resource "azurerm_service_plan" "AzurermServicePlan" {
 }
 
 ###############
-# Azure: Database
+# Azure: Postgresql Database
 ###############
 
 resource "azurerm_postgresql_server" "postgresql-server" {
-  name                = "postgresql-server-1"
-  location            = azurerm_resource_group.TPAzureGroup.location
-  resource_group_name = azurerm_resource_group.TPAzureGroup.name
-
-  sku_name = "B_Gen5_2"
-
-  storage_mb                   = 5120
+  name                         = "pgsql-serv-1"
+  location                     = azurerm_resource_group.TPAzureGroup.location
+  resource_group_name          = azurerm_resource_group.TPAzureGroup.name
+  sku_name                     = "GP_Gen5_4"
+  version                      = "11"
+  storage_mb                   = 640000  
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
   auto_grow_enabled            = true
-
+  
   administrator_login          = data.azurerm_key_vault_secret.postgres-username.value
   administrator_login_password = data.azurerm_key_vault_secret.postgres-password.value
-  version                      = "9.5"
-  ssl_enforcement_enabled      = true
+  ssl_enforcement_enabled      = false
+  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
 }
 
-
-resource "azurerm_mssql_firewall_rule" "postgresql-rule" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_postgresql_server.postgresql-server.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
+resource "azurerm_postgresql_firewall_rule" "pgsql-firewall-rule" {
+  name                = "AllowAzureServices"
+  resource_group_name = azurerm_resource_group.TPAzureGroup.name
+  server_name         = azurerm_postgresql_server.postgresql-server.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
 
 resource "azurerm_postgresql_database" "postgresql-database" {
@@ -82,13 +81,77 @@ resource "azurerm_linux_web_app" "AzurermWebApp" {
   service_plan_id     = azurerm_service_plan.AzurermServicePlan.id
   site_config {
     application_stack {
-      dotnet_version = "6.0"
+      node_version = "16-lts"
     }
   }
+}
 
-  connection_string {
-    name = "DefaultConnection"
-    value = "Server=tcp:${azurerm_mssql_server.sql-srv.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql-db.name};Persist Security Info=False;User ID=${data.azurerm_key_vault_secret.database-username.value};Password=${data.azurerm_key_vault_secret.database-password.value};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-    type = "SQLAzure"
+###############
+# PGAdmin : Container Instance
+###############
+resource "azurerm_container_group" "rabbitmq" {
+  name                = "aci-pgadmin-${var.projectName}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-vclarke.name
+  location            = data.azurerm_resource_group.rg-vclarke.location
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-pgadmin-${var.projectName}${var.environment_suffix}"
+  os_type             = "Linux"
+
+  container {
+    name   = "pgadmin"
+    image  = "dpage/pgadmin4"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 5672
+      protocol = "TCP"
+    }
+
+    ports {
+      port     = 15672
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      "PGADMIN_DEFAULT_EMAIL" = data.azurerm_key_vault_secret.pgadmin-username.value,
+      "PGADMIN_DEFAULT_PASSWORD" = data.azurerm_key_vault_secret.pgadmin-password.value
+    }
+  }
+}
+
+###############
+# API : Container Instance
+###############
+resource "azurerm_container_group" "api" {
+  name                = "aci-api-${var.projectName}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-vclarke.name
+  location            = data.azurerm_resource_group.rg-vclarke.location
+  ip_address_type     = "None"
+  dns_name_label      = "aci-api-${var.projectName}${var.environment_suffix}"
+  os_type             = "Linux"
+
+  container {
+    name   = "nodeapi"
+    image  = "greugreu/nodeapi:1.0.4"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 3000
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      DB_HOST: postgres
+      DB_USERNAME = "${data.azurerm_key_vault_secret.postgres-username.value}@${azurerm_postgresql_server.postgresql-server.name}"
+      DB_PASSWORD: data.azurerm_key_vault_secret.postgres-password
+      DB_DATABASE: 'db'
+      DB_DAILECT: 'postgres'
+      DB_PORT: 5432
+      DATABASE_URL: "postgres://${data.azurerm_key_vault_secret.postgres-username}:${data.azurerm_key_vault_secret.postgres-password}@postgres:5432/db"
+      NODE_ENV: development
+      PORT: 3000
+    }
   }
 }
